d7d379948c41cda80db4994ea03c09f080485f66
AathiKrishna-JR <aathi3612@gmail.com>
2025-02-03 12:38:16 +0530

    1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-tasks/no-task.component";
+import { UserTasksComponent } from "./users /users-tasks/user-tasks.component";
 
 export const routes : Routes = [
     {
@@ -9,8 +10,8 @@ export const routes : Routes = [
     }
     ,{
         
-            path : 'tasks' ,
-            component :TasksComponent,
+            path : 'users/:userId',
+            component :UserTasksComponent,
         
     }
 ]
