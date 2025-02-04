c69a183d2b78c9710eac4738fe8a6202b4f4808e
AathiKrishna-JR <aathi3612@gmail.com>
2025-02-04 11:19:15 +0530

    gaurd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,21 @@
-import { Routes } from "@angular/router";
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-tasks/no-task.component";
 import { UserTasksComponent, resolveTitle, resolveUserName } from "./users /users-tasks/user-tasks.component";
 import { NewTaskComponent } from "./tasks/new-tasks/new-task.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import {routes as userRoutes} from "./users /users.routes"
+import { inject } from "@angular/core";
+
+const dummyCanMatch : CanMatchFn = (route,segments) => {
+    const router = inject(Router);
+    const shouldGetAccess = Math.random();
+    if(shouldGetAccess < 0.5){
+        return true;
+    }
+    return new RedirectCommand(router.parseUrl('/unauthorized'));
+}
+
 export const routes : Routes = [
     {
         path : '',
@@ -17,6 +28,7 @@ export const routes : Routes = [
         
             path : 'users/:userId',
             component :UserTasksComponent,
+            canMatch: [dummyCanMatch],
             children: userRoutes,
             data : {
                 message : 'Hello!'
