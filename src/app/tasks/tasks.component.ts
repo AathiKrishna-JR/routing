5e1b9ce95aaf00a90d9e953fffaa927997458a47
AathiKrishna-JR <aathi3612@gmail.com>
2025-02-04 09:32:48 +0530

    1

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, computed, inject, input } from '@angular/core';
+import { Component, DestroyRef, OnInit, computed, inject, input } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
@@ -12,17 +12,23 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
   styleUrl: './tasks.component.css',
   imports: [TaskComponent,RouterLink],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit{
+ 
 
   private tasksService = inject(TasksService);
   userId = input.required<string>();
-  order = input<'asc' | 'desc'>('asc');
- 
+ // order = input<'asc' | 'desc'>('asc');
+ order ?: 'asc'|'desc';
   private  activatedRoute = inject(ActivatedRoute);
   userTasks = computed(() => this.tasksService.allTasks().filter(task => task.userId === this.userId()));
-  //private d
- 
+ private destroyRef = inject(DestroyRef);
   
-
+private activated = inject(ActivatedRoute);
+ngOnInit(): void {
+ const subs = this.activatedRoute.queryParams.subscribe({
+  next : params => this.order = params['order']
+ });
+ this.destroyRef.onDestroy(() =>subs.unsubscribe());
+}
 
 }
